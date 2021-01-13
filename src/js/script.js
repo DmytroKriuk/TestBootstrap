5f4d220680fdba5dc7aa36a9902e6c81b17c424c
Dmytro Kriuk <dmytro.kryuk@gmail.com>
2021-01-13 12:14:58 +0200

    feat add sections works and testimonials

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('.owl-carousel').owlCarousel({
+  $('.works-carousel').owlCarousel({
     loop:true,
     margin:30,
     nav:true,
@@ -14,6 +14,32 @@ $(function() {
         },
         1000:{
             items:1.3
+        }
+    }
+  });
+
+  $('ul.tabs__caption').on('click', 'li:not(.active)', function(e) {
+    e.preventDefault();
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      .closest('section.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+  });
+
+  $('.contributor-carousel').owlCarousel({
+    loop:true,
+    margin:5,
+    nav:true,
+    dotts: false,
+    navText: ['<img src="img/left.svg">', '<img src="img/right.svg">'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
         }
     }
   });
